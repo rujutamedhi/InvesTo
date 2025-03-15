2c5514e330ac5a8f9942e4cc31ee261ba1590231
rujutamedhi <rujutamedhi@gmail.com>
2025-03-15 18:07:55 +0530

    table code

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Route, Routes} from 'react-router-dom'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Optional 404 page
+import "./App.css";
+import Analyticsdashboard from "../src/components/table";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <Router>
       <Routes>
-        <Route/>
+        <Route path="/" element={<Analyticsdashboard />} />
+        <Route path="/table" element={<Analyticsdashboard />} />
+        
       </Routes>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
