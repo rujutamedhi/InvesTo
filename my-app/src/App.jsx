096274d5ed0cbd234ffd6145a3d7703e7b856bc0
ShravaniAnilPatil <shravanipatil1427@gmail.com>
2025-03-16 06:24:07 +0530

    changes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,34 +1,29 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Profile from "./components/Profile"; 
-import Sidebar from "./components/Sidebar"; // Import Sidebar
+import Sidebar from "./components/Sidebar"; 
 import SignupForm from "./components/SignUp";
 import {  AuthProvider } from "./context/AuthContext";
 import Login from "./components/Login";
 import "./App.css";
-import StockDetails from "./pages/StockDetails";
-import StockList from "./pages/StockList";
-import BondsList from "./pages/BondList";
-import Table from "./components/table"
-import Collabform from "./components/CollabForm"
+
 function Layout() {
-  const location = useLocation(); // Get current route
-  const showSidebar = location.pathname !== "/"; // Hide sidebar on Home page
+  const location = useLocation(); 
+  const showSidebar = location.pathname !== "/"; 
 
   return (
-    <div className="app-container" >
-      <Sidebar/>
-      {/* <Navbar />
-      
-      <div className="main-content">
-        {showSidebar && <Sidebar />} Show Sidebar except on Home */}
+    <div className="app-container">
+
         <div className="page-content">
           <AuthProvider>
           <Routes>
             {/* <Route path="/" element={<Home />} /> */}
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/profile" element={<Profile />} />
-          </Routes>
+            <Route path="/signup" element={<SignupForm/>} />
+            <Route path="/login" element={<Login/>} />
+            </Routes>
+            </AuthProvider>
         </div>
       </div>
     // </div>
